d974cc872c9d5eb284759778263778b3eb5f4d3f
Grace Casale <gracecasale@gmail.com>
2018-10-19 19:04:50 -0400

    added the formatted date below the time

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const clock = document.querySelector('#clock');
 const display = clock.querySelector('#display');
 const hours = display.querySelector('#hours');
 const minutes = display.querySelector('#minutes');
+const date = clock.querySelector('#date');
 let time = new Date();
 
 //event bindings
@@ -10,13 +11,26 @@ let time = new Date();
 
 //functions
 function tick() {
+    //get time
     const minValue = time.getMinutes();
     const hourValue = time.getHours();
+    //render time
     hours.textContent = hourValue; 
     minutes.textContent = `${minValue}`.padStart(2, '0');
-    time = new Date(); 
+   
+    //get date (format: MM-DD-YYYY)
+    const year = time.getFullYear();
+    const month = time.getMonth();
+    const day = time.getDate();
+
+    //render date
+    date.textContent = `${month}-${day}-${year}`; 
+
+     //update time and date
+     time = new Date();
 }
 
 //kick it off
 tick();
 setInterval(tick, 1000); 
+
