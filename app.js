34ad785a1cd238c11e38fbfe46aa60217ff8dc65
Grace Casale <gracecasale@gmail.com>
2018-12-12 19:41:20 -0500

    reloaded all pages in order to find the bug putting clock off center

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-//variable declarations
+// variable declarations
 const clock = document.querySelector('#clock');
 const display = clock.querySelector('#display');
 const hours = display.querySelector('#hours');
 const minutes = display.querySelector('#minutes');
 const date = clock.querySelector('#date');
-const amPm = display.querySelector('#am-pm'); 
+const amPm = display.querySelector('#am-pm');
 let time = new Date();
-let isPm = time.getHours() >= 12; 
-//event bindings
+let isPm = time.getHours() >= 12;
+// event bindings
 
-//functions
+// functions
 function tick() {
     //get time
     const minValue = time.getMinutes();
     const hourValue = time.getHours();
     //render time
-    hours.textContent = hourValue % 12 === 0 ? 12 : `${hourValue % 12}`.padStart(2, '0'); 
+    hours.textContent = hourValue % 12 === 0 ? 
+                        12 : 
+                        `${hourValue % 12}`.padStart(2, '0');
     minutes.textContent = `${minValue}`.padStart(2, '0');
 
     //get date (format: MM-DD-YYYY)
-    const year = `${time.getFullYear()}`.padStart(4,'0');
-    const month = `${time.getMonth() +1}`.padStart(2, '0');
+    const year = `${time.getFullYear()}`.padStart(4, '0');
+    const month = `${time.getMonth() + 1}`.padStart(2, '0');
     const day = `${time.getDate()}`.padStart(2, '0');
-
     //render date
     date.textContent = `${month}-${day}-${year}`;
-
     //render am/pm status
-    amPm.classList.remove('am','pm'); 
-    if (isPm) {
+    amPm.classList.remove('am', 'pm');
+    if(isPm) {
         amPm.classList.add('pm');
     } else {
         amPm.classList.add('am');
     }
-
     //update time and date
     time = new Date();
     //update am/pm
@@ -43,4 +42,3 @@ function tick() {
 //kick it off
 tick();
 setInterval(tick, 1000);
-
